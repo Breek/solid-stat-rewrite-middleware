0ed57109a298a0208e2e768e36f2f4660c6688a6
Jérémy Chatard <jchatard@breek.fr>
2022-12-29 10:13:15 +0100

    Add the Rewrite middleware.

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -4,6 +4,9 @@ import {
   StartServer,
 } from "solid-start/entry-server";
 
+import { useRewriteMiddleware } from "~/middlewares/rewrite";
+
 export default createHandler(
+  useRewriteMiddleware,
   renderAsync((event) => <StartServer event={event} />)
 );
---
+++ src/middlewares/rewrite.tsx
@@ -0,0 +1,51 @@
+import { MiddlewareInput } from "solid-start/entry-server/StartServer";
+import { FETCH_EVENT, FetchEvent, PageEvent } from "solid-start/server/types";
+import { internalFetch } from "solid-start/api/internalFetch";
+
+export const useRewriteMiddleware = ({ forward }: MiddlewareInput) => {
+  return async (event: FetchEvent) => {
+    const pathname = new URL(event.request.url).pathname;
+    console.debug("🏣 Requested URL path", pathname);
+
+    // We want to rewrite `/about` to `/about-company`
+    if (pathname === "/about") {
+      console.debug("🛫 We want to rewrite `/about` to `/about-company`");
+      // Let's create a new URL
+      const url = new URL(`/about-company`, "http://internal");
+      const request = new Request(url.href);
+
+      const prevPath = event.request.headers.get("x-solid-referrer");
+
+      let statusCode = 200;
+      function setStatusCode(code: number) {
+        statusCode = code;
+      }
+      function getStatusCode() {
+        return statusCode;
+      }
+
+      let responseHeaders = new Headers({
+        "Content-Type": "text/html",
+      });
+
+      // We create a new FetchEvent so that the router thinks we are requesting `/about-company`
+      const newFetchEvent: PageEvent = Object.freeze({
+        request: request,
+        prevUrl: prevPath || "",
+        routerContext: {},
+        tags: [],
+        env: event.env,
+        $type: FETCH_EVENT,
+        responseHeaders,
+        setStatusCode: setStatusCode,
+        getStatusCode: getStatusCode,
+        fetch: internalFetch,
+      });
+
+      return forward(newFetchEvent);
+    }
+
+    // We don't need to rewrite this path, so we can just forward the event
+    return forward(event);
+  };
+};
